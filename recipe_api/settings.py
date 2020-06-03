4adad04cc9163973fa2cc23d3f40920b1fe3de06
Manav Parmar <parmar.manav2000@gmail.com>
2020-06-03 13:23:46 +0530

    Added user create, auth and manage endpoint

--- recipe_api/settings.py
+++ recipe_api/settings.py
@@ -38,7 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'core'
+    'rest_framework',
+    'rest_framework.authtoken',
+
+    'core',
+    'user',
 ]
 
 MIDDLEWARE = [
@@ -81,7 +85,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
